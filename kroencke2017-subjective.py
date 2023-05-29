0be790e38ce1af62d2455b07edcef9e12c2917c2
Lumin <lwg342@gmail.com>
2023-05-29 13:11:40 +0100

    Switch to particles package for SMC implementation with example.

--- kroencke2017-subjective.py
+++ kroencke2017-subjective.py
@@ -3,6 +3,14 @@ import numpy as np
 from scipy.optimize import minimize
 from wlpy.covariance import hac_cov
 import pandas as pd
+from utils import (
+    MCMCBase,
+    AdaptiveSampler,
+    VectorMCMC,
+    VectorMCMCKernel,
+    BaseLogLike,
+    uniform,
+)
 
 
 def residual_function(params, data):
@@ -99,13 +107,55 @@ y2 = [objective_function(np.array([-0.001, i]), data) for i in x]
 plt.plot(x, y)
 plt.plot(x, y2)
 
-# %% 
+
+# %%
 def probability_divergence(p, eta=0):
     if eta == 0:
         return np.mean(p * np.log(p))
     else:
         raise NotImplementedError("Not implemented yet")
-    
-M = np.exp(result.x[0] * residual_function(result.x, data)[0])/np.mean(np.exp(result.x[0] * residual_function(result.x, data)[0]))
+
+
+M = np.exp(result.x[0] * residual_function(result.x, data)[0]) / np.mean(
+    np.exp(result.x[0] * residual_function(result.x, data)[0])
+)
 print(f"Probability divergence: {probability_divergence(M)}")
+
+
 # %%
+class NewLogLikelihood(BaseLogLike):
+    def __init__(self, model, data, args):
+        """
+        Likelihood function for data with additive, iid errors sampled from a
+        normal distribution with mean = 0 and std_dev = args. If args is None,
+        assumes that the last column of inputs contains the std_dev value.
+        """
+        super().__init__(model, data, args)
+
+    def __call__(self, inputs):
+        std_dev = self._args
+        if std_dev is None:
+            std_dev = inputs[:, -1]
+            inputs = inputs[:, :-1]
+        var = std_dev**2
+
+        output = self._get_output(inputs)
+
+        return self._calc_normal_log_like(output, self._data, var)
+
+    @staticmethod
+    def _calc_normal_log_like(output, data, var):
+        ssqe = np.sum((output - data) ** 2, axis=1)
+
+        term1 = -np.log(2 * np.pi * var) * (output.shape[1] / 2.0)
+        term2 = -1 / 2.0 * ssqe / var
+
+        return term1 + term2
+
+
+def eval_model(theta):
+    # time.sleep(0.1) # artificial slowdown to show off progress bar
+    a = theta[:, 0, None]
+    b = theta[:, 1, None]
+    c = theta[:, 2, None]
+    return a + x * b + x**2 * c
